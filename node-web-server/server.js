3ed3132297aca08b5b6ab9405c03a399f6252e78
J Michael Thurman <jmt@jmichaelthurman.com>
2018-07-06 07:20:21 -0500

    add start command and port is now reading ENV

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var hbs = require('hbs');
-
+var fs = require('fs');
 var app = express();
+
+const port = process.env.PORT || 3000;
+console.log(`port: ${port}`);
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear', ()=>{
@@ -9,31 +12,50 @@ hbs.registerHelper('getCurrentYear', ()=>{
 });
 
 app.set('view engine','hbs');
+//Middleware
+
+
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.ip} ${req.method} ${req.url} ${req.protocol}`
+
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err)=>{
+    if(err){
+    console.log('Unable to write to server.log.');
+  }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs')
+// });
 app.use(express.static(__dirname + '/public'));
 //Route handlers
 
 
-app.get('/',(request, response)=>{
-  response.render('home.hbs',{
+app.get('/',(req, res)=>{
+  res.render('home.hbs',{
     siteName: 'My Site',
     pageTitle: 'Home',
     welcomeMessage: 'Welcome to my site.'
   })
 });
 
-app.get('/about',(request,response)=>{
-  response.render('about.hbs',{
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
     pageTitle: 'About',
   })
 });
 
-app.get('/bad',(request,response)=>{
-  response.send({
+app.get('/bad',(req,res)=>{
+  res.send({
     status: 'Bad Request',
     status_code: 400,
     errorMessage: 'Unable to process bad request(400).'
   })
 })
-app.listen(3000,()=>{
-  console.log('Server is listening on port 3000')
+app.listen(port,()=>{
+  console.log(`Server is listening on port ${port}`)
 });
