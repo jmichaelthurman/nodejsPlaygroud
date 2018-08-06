9ea6e1ad90e533f6814ee860271b53165c29fb9b
J Michael Thurman <jmt@jmichaelthurman.com>
2018-08-05 22:43:57 -0500

    added PATCH route

--- toDoApi/server/server.js
+++ toDoApi/server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -68,5 +69,27 @@ app.listen(port,()=> {
   console.log(`Server started. Listening on port ${port}.`);
 });
 
+app.patch('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
 
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send({'Error': 'ID not valid.'});
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send({'Error':'ID not found.'})
+    }
+    return res.status(200).send({todo});
+  }, (err)=>{
+    res.status(400).send(err);
+  });
+});
 module.exports = {app};
