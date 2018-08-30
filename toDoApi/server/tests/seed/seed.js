fdb4c623b329f17ecc6c306f6011a8581f32139e
J Michael Thurman <jmt@jmichaelthurman.com>
2018-08-29 19:32:29 -0500

    localhost >> IP address in mongoCDB to fix timeout error in test suite.

--- toDoApi/server/tests/seed/seed.js
+++ toDoApi/server/tests/seed/seed.js
@@ -1,5 +1,26 @@
+const jwt = require('jsonwebtoken');
 const {ObjectID} = require('mongodb');
 const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const user1ID = new ObjectID();
+const user2ID = new ObjectID();
+
+const users =[{
+    _id: user1ID,
+    email: 'me@me.com',
+    password: 'thatOnePassword',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: user1ID, access:'auth'}, 'somesecret').toString()
+    }]
+},{
+    _id: user2ID,
+    email: 'her@here.com',
+    password: 'pa$$word'
+
+}]
+
 
 const todos = [{
     _id: new ObjectID(),
@@ -24,4 +45,20 @@ const todos = [{
       }).then(() => done());
   };
 
-  module.exports = { todos, populateTodos};
+// const populateUsers = (done) => {
+//     User.remove({}).then(() => {
+//         var userOne = new User(users[0]).save();
+//         var userTwo = new User(users[1]).save();
+//        return Promise.all([userOne, userTwo]).then(()=>done());
+//     });
+// };
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+      var userOne = new User(users[0]).save();
+      var userTwo = new User(users[1]).save();
+  
+      return Promise.all([userOne, userTwo])
+    }).then(() => done());
+  };
+
+  module.exports = { todos, populateTodos, users, populateUsers};
